8bc0c1f58c3ef454d54c1acca8eb0271cd58e91f
ayda76 <aydavahedfrough@yahoo.com>
2026-02-27 06:17:48 +0000

    added tests to product and profile

--- backend/profileuser/tests.py
+++ backend/profileuser/tests.py
@@ -1,5 +1,19 @@
 from django.test import TestCase, Client
-
+from django.contrib.auth.models import User
+from .models import *
 # Create your tests here.
 
-
+    
+class TestProfile(TestCase):
+    def setUp(self):
+        self.user=User.objects.create_user(username='testuser', password='testpassword')
+        
+    def test_model_profile(self):
+        Profile.objects.create(
+            user=self.user,
+            firstname='testProfile',
+            lastname='testlastname',
+            phone='09147689387',
+            address='uuuuuu kioo lskslslk hgccccc',
+            email='test@gmail.com'
+        )    
